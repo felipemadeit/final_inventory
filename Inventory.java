ebdf63f7b3012b16e1ba1e15f0dd473e03e04580
felipemadeit <felipemadeit@gmailcom>
2024-06-04 15:36:42 -0500

    Last Commit

--- Inventory.java
+++ Inventory.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -14,8 +18,8 @@ public class Inventory {
 
     // Constructors, getter, setter and toString
 
-    
     public Inventory() {
+        this.inventory = new ArrayList<>();
     }
 
     public Inventory(String name, int quantityProducts, String description, ArrayList<Product> inventory) {
@@ -69,114 +73,226 @@ public class Inventory {
         return sb.toString();
     }
 
+    // Methods of the class
+
     /**
      * This method create a txt file with the inventory
      * 
-     * @param txtName the name of the file 
-     * @param  nameInv the name of the inventory
+     * @param txtName the name of the file
+     * @param nameInv the name of the inventory
      * @param descrip the description of the inventory
      */
-
-    public void createTXT (String txtName, String nameInv, String descrip) {
+    public void createTXT(String txtName, String nameInv, String descrip) {
 
         try {
-
-            String pathSave = "inventoryTxtFiles" +  "/" + txtName;
+            // try to create a txt and save the data of the inventory
+            String pathSave = "inventoryTxtFiles" + "/" + txtName;
             FileWriter writer = new FileWriter(pathSave);
 
-            writer.write(nameInv+"\n");
-            writer.write(descrip +"\n");
+            writer.write(nameInv + "\n");
+            writer.write(descrip + "\n");
             writer.close();
 
         } catch (IOException e) {
-
             System.out.println("Error can not create a txt file");
         }
-
-        
-
     }
 
     /**
      * This method append to the inventory txt the products
      * 
-     * @param txtName the name of the file to append the products
-     * @param name the name of the product                                           
-     * @param quantity the quantity of the product
+     * @param txtName     the name of the file to append the products
+     * @param name        the name of the product
+     * @param quantity    the quantity of the product
      * @param description the description of the product
-     * 
      */
+    public void appendToTxt(String txtName, String name, int quantity, String description) {
 
-     public void appendToTxt(String txtName, String name, int quantity, String description) {
-        
         // The path of the file to append the products
         String filePath = txtName;
 
-
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
-        
+
             // Write in the file the product data
             writer.write(name + "\n");
             writer.write(quantity + "\n");
             writer.write(description + "\n");
-        
-            System.out.println("Successfully appended product to " + filePath);
+
+            System.out.println("Correctly created product");
+
         } catch (IOException e) {
-        
+
             System.err.println("An error occurred while appending to the file: " + e.getMessage());
         }
     }
 
     /**
-     * This method get the data from the user to create an add products to the inventory
+     * This method get the data from the user to create an add products to the
+     * inventory
+     * This method use the appendToTxt method
      * 
      * @param inventoryName
-     * @return an arraylist with Products
+     * @param fileName
+     * @return products an arraylist with Products
      */
-    public ArrayList<Product> addProducts (String inventoryName, String fileName) {
+    public ArrayList<Product> addProducts(String inventoryName, String fileName) {
 
         // Create an arraylist to storage the products
         ArrayList<Product> products = new ArrayList<>();
 
         System.out.println("Enter the quantity of the products");
         int productQuantity = scanner.nextInt();
+        scanner.nextLine();
 
         // Get quantity times the product data
         for (int i = 0; i < productQuantity; i++) {
 
             System.out.println("Enter the name of the product: ");
-            String productName = scanner.next();
-
+            String productName = scanner.nextLine();
+            System.out.println();
 
             System.out.println("Please enter the quantity of the " + productName);
             int quantityProd = scanner.nextInt();
             scanner.nextLine();
+            System.out.println();
 
-            System.out.println("Please enter a short description for the " + productName+ " product");
+            System.out.println("Please enter a short description for the " + productName + " product");
             String productDescription = scanner.nextLine();
-
-            System.out.println(productName);
-            System.out.println(quantityProd);
-            System.out.println(productDescription);
+            System.out.println();
 
             String pathFile = "inventoryTxtFiles" + "/" + fileName;
 
             appendToTxt(pathFile, productName, quantityProd, productDescription);
 
-            // Create a new product 
+            // Create a new product
             Product newProduct = new Product(productName, quantityProd, productDescription);
-            
+
             // Add a the product to the products array
             products.add(newProduct);
-            
+
+            System.out.println();
+            System.out.println();
+
+            // If is the last product
+            if (i == productQuantity - 1) {
+                System.out.println();
+                System.out.println("You have added " + productQuantity + " products to your inventory ");
+            }
         }
+
         // Return the products array
         return products;
     }
 
+    public static File[] getListFiles(File folder) {
 
+        File[] txtFiles = new File[0];
 
+        if (folder.isDirectory()) {
+            FilenameFilter filterOnlyTxt = (dir, name) -> name.toLowerCase().endsWith(".txt");
+            txtFiles = folder.listFiles(filterOnlyTxt);
+        }
+        if (txtFiles != null) {
+            System.out.println("Inventory quantity: " + txtFiles.length);
+        } else {
+            System.out.println("The folder does not exist or is empty");
+        }
 
-    
-    
+        return txtFiles;
+    }
+
+    /**
+     * This method get the quantity of the inventories 
+     * 
+     * @return inventoryLoaded 
+     */
+    public static Inventory chargeQuantityFiles(File txtFile) {
+
+        Inventory inventoryLoaded = null;
+
+        if (txtFile != null) {
+            try (BufferedReader br = new BufferedReader(new FileReader(txtFile))) {
+                // Read the txt data 
+                String inventoryNm = br.readLine();
+                String inventoryDs = br.readLine();
+
+                inventoryLoaded = new Inventory();
+                inventoryLoaded.setName(inventoryNm);
+                inventoryLoaded.setDescription(inventoryDs);
+
+                ArrayList<Product> products = new ArrayList<>();
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    String productName = line;
+                    int productQuanti = Integer.parseInt(br.readLine());
+                    String productDesc = br.readLine();
+
+                    Product productToLoad = new Product(productName, productQuanti, productDesc);
+                    products.add(productToLoad);
+                }
+
+                inventoryLoaded.setInventory(products);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("The txt has no data");
+        }
+
+        return inventoryLoaded;
+    }
+
+    /**
+     * This method updates the quantity of a specific product
+     * 
+     * @param productName the name of the product
+     * @param newQuantity the new quantity to set
+     */
+    public void updateProductQuantity(String productName, int newQuantity) {
+        for (Product product : inventory) {
+            if (product.getName().equalsIgnoreCase(productName)) {
+                product.setQuantity(newQuantity);
+                System.out.println("Quantity of " + productName + " updated to " + newQuantity);
+                return;
+            }
+        }
+        System.out.println("Product not found in inventory");
+    }
+
+    /**
+     * This method removes a specific product from the inventory
+     * 
+     * @param productName the name of the product to remove
+     */
+    public void removeProduct(String productName) {
+        for (Product product : inventory) {
+            if (product.getName().equalsIgnoreCase(productName)) {
+                inventory.remove(product);
+                System.out.println("Product " + productName + " removed from inventory");
+                return;
+            }
+        }
+        System.out.println("Product not found in inventory");
+    }
+
+    /**
+     * This method changes the name of the inventory
+     * 
+     * @param newName the new name to set
+     */
+    public void updateInventoryName(String newName) {
+        this.name = newName;
+        System.out.println("Inventory name updated to " + newName);
+    }
+
+    /**
+     * This method updates the description of the inventory
+     * 
+     * @param newDescription the new description to set
+     */
+    public void updateInventoryDescription(String newDescription) {
+        this.description = newDescription;
+        System.out.println("Inventory description updated to " + newDescription);
+    }
 }
--- Main.java
+++ Main.java
@@ -1,48 +1,77 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     static Scanner scanner = new Scanner(System.in);
+    static HashMap<String, Integer> users = new HashMap<>();
+    static String currentUser;
 
     public static void main(String[] args) {
 
+        // Add some predefined users (for testing purposes)
+        users.put("user1", 0);
+        users.put("user2", 0);
 
+        // Perform login
+        if (!login()) {
+            System.out.println("Invalid login. Exiting program.");
+            return;
+        }
 
         int optionMenu = 0;
 
-
-        while (optionMenu != 10) {
+        while (optionMenu != 3) {
 
             // Software menu
-            System.out.println("Welcome to the best inventory software");
-            System.out.println("What do you want to do today ?");
+            System.out.println();
+            System.out.println("What do you want to do today, " + currentUser + "?");
+            System.out.println();
             System.out.println("1. Create new inventory");
+            System.out.println();
+            System.out.println("2. View and modify inventories");
+            System.out.println();
+            System.out.println("3. Exit");
             System.out.println("Select an option: ");
 
+            // Get the user option 
             optionMenu = scanner.nextInt();
-            
+            scanner.nextLine();
 
             switch (optionMenu) {
+
+                // The first case is for creating a new inventory 
                 case 1 -> {
 
                     // Create a new inventory
                     System.out.println("Enter the name of the inventory");
-                    String nameInv = scanner.next();
-                    scanner.nextLine();
+                    String nameInv = scanner.nextLine();
+                    System.out.println();
 
-                    // The inventory will saved in a txt file
+                    // The inventory will be saved in a txt file
                     String txtName = nameInv + ".txt";
 
-                    System.out.println("Enter a short description to " + nameInv + " inventory");
+                    System.out.println("Enter a short description for the " + nameInv + " inventory");
                     String descrip = scanner.nextLine();
+                    System.out.println();
 
                     Inventory inventoryO = new Inventory();
     
-                    System.out.println("Do you want to add products to your " +nameInv + " inventory ?" );
+                    // After creating the inventory the user can choose whether to add products or not
+                    System.out.println("Do you want to add products to your " + nameInv + " inventory?");
+                    System.out.println();
                     System.out.println("1. Add Products");
-                    System.out.println("2. I don´t want to add products");
+                    System.out.println();
+                    System.out.println("2. I don't want to add products");
+                    System.out.println();
 
                     int productsOption = scanner.nextInt();
+                    scanner.nextLine();
 
                     // Create a new object inventory with the data
                     inventoryO.setName(nameInv);
@@ -51,27 +80,179 @@ public class Main {
                     // Create the inventory txt
                     inventoryO.createTXT(txtName, nameInv, descrip);
 
-
                     if (productsOption == 1) {
-
                         inventoryO.setInventory(inventoryO.addProducts(nameInv, txtName));
-
                     } else {
-                        
-                        System.out.println("Your " + nameInv + " inventory was created succesfully");
+                        System.out.println("Your " + nameInv + " inventory was created successfully");
                     }
-   
 
+                    // Update the user's inventory count
+                    users.put(currentUser, users.get(currentUser) + 1);
+                }
+
+                case 2 -> {
                     
+                    // Array to save the inventories
+                    List<Inventory> loadedInventories = new ArrayList<>();
 
-                   
-                    
+                    Inventory inventoriesToLoad = new Inventory();
 
+                    File folder = new File("inventoryTxtFiles");
+                    File[] listTxtFiles = inventoriesToLoad.getListFiles(folder);
+
+                    if (listTxtFiles != null) {
+                        for (File txtFile : listTxtFiles) {
+                            Inventory newInventory = inventoriesToLoad.chargeQuantityFiles(txtFile);
+                            if (newInventory != null) {
+                                // Add the inventory to the list 
+                                loadedInventories.add(newInventory);
+                            }
+                        }
+                        System.out.println("Inventories: ");
+                        System.out.println();
+                        for (int i = 0; i < loadedInventories.size(); i++) {
+                            Inventory inventory = loadedInventories.get(i);
+                            System.out.println((i + 1) + ". Inventory name: " + inventory.getName());
+                        }
+                        System.out.println("Select an inventory to view or modify (Enter the number): ");
+                        int inventoryIndex = scanner.nextInt() - 1;
+                        scanner.nextLine();
+
+                        if (inventoryIndex >= 0 && inventoryIndex < loadedInventories.size()) {
+                            Inventory selectedInventory = loadedInventories.get(inventoryIndex);
+                            System.out.println("Selected Inventory: " + selectedInventory.getName());
+                            System.out.println("Description: " + selectedInventory.getDescription());
+                            System.out.println("Products:");
+                            for (Product product : selectedInventory.getInventory()) {
+                                System.out.println("    -> " + product.getName());
+                                System.out.println("        Description: " + product.getDescription());
+                                System.out.println("        Quantity: " + product.getQuantity());
+                                System.out.println();
+                            }
+
+                            int modifyOption = 0;
+                            while (modifyOption != 6) {
+                                System.out.println("What would you like to do?");
+                                System.out.println("1. Add a product");
+                                System.out.println("2. Remove a product");
+                                System.out.println("3. Update product quantity");
+                                System.out.println("4. Change inventory name");
+                                System.out.println("5. Change inventory description");
+                                System.out.println("6. Go back");
+                                modifyOption = scanner.nextInt();
+                                scanner.nextLine();
+
+                                switch (modifyOption) {
+                                    case 1 -> {
+                                        // Add a new product
+                                        System.out.println("Enter product name: ");
+                                        String productName = scanner.nextLine();
+                                        System.out.println("Enter product quantity: ");
+                                        int productQuantity = scanner.nextInt();
+                                        scanner.nextLine();
+                                        System.out.println("Enter product description: ");
+                                        String productDescription = scanner.nextLine();
+
+                                        selectedInventory.getInventory().add(new Product(productName, productQuantity, productDescription));
+                                        selectedInventory.appendToTxt("inventoryTxtFiles/" + selectedInventory.getName() + ".txt", productName, productQuantity, productDescription);
+                                        System.out.println("Product added successfully.");
+                                    }
+                                    case 2 -> {
+                                        // Remove a product
+                                        System.out.println("Enter product name to remove: ");
+                                        String productName = scanner.nextLine();
+                                        selectedInventory.removeProduct(productName);
+                                        updateInventoryFile(selectedInventory);
+                                    }
+                                    case 3 -> {
+                                        // Update product quantity
+                                        System.out.println("Enter product name to update quantity: ");
+                                        String productName = scanner.nextLine();
+                                        System.out.println("Enter new quantity: ");
+                                        int newQuantity = scanner.nextInt();
+                                        scanner.nextLine();
+                                        selectedInventory.updateProductQuantity(productName, newQuantity);
+                                        updateInventoryFile(selectedInventory);
+                                    }
+                                    case 4 -> {
+                                        // Change inventory name
+                                        System.out.println("Enter new inventory name: ");
+                                        String newName = scanner.nextLine();
+                                        selectedInventory.updateInventoryName(newName);
+                                        updateInventoryFile(selectedInventory);
+                                    }
+                                    case 5 -> {
+                                        // Change inventory description
+                                        System.out.println("Enter new inventory description: ");
+                                        String newDescription = scanner.nextLine();
+                                        selectedInventory.updateInventoryDescription(newDescription);
+                                        updateInventoryFile(selectedInventory);
+                                    }
+                                    case 6 -> System.out.println("Going back to main menu.");
+                                    default -> System.out.println("Invalid option. Please try again.");
+                                }
+                            }
+                        } else {
+                            System.out.println("Invalid inventory selection.");
+                        }
+                    } else {
+                        System.out.println("There are no files in the folder 'inventoryTxtFiles'");
+                    }
+                    break;
                 }
             }
         }
+    }
 
-        
-        
+    public static boolean login() {
+        System.out.println("Please select an option: ");
+        System.out.println("1. Create User");
+        System.out.println("2. Login");
+        int optionUser = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline left-over
+
+        switch (optionUser) {
+            case 1:
+                System.out.println("Enter your username: ");
+                String newUserName = scanner.nextLine();
+                if (users.containsKey(newUserName)) {
+                    System.out.println("Username already exists. Try logging in.");
+                } else {
+                    users.put(newUserName, 0);
+                    System.out.println(newUserName + " has been registered.");
+                }
+                return login(); // Call login again to either create another user or login
+            case 2:
+                System.out.println("Enter username: ");
+                String username = scanner.nextLine();
+
+                if (users.containsKey(username)) {
+                    currentUser = username;
+                    return true;
+                } else {
+                    System.out.println("Username not found.");
+                    return false;
+                }
+            default:
+                System.out.println("Invalid option. Please try again.");
+                return login();
+        }
+    }
+
+    // Method to update the inventory file after any modification
+    public static void updateInventoryFile(Inventory inventory) {
+        try {
+            FileWriter writer = new FileWriter("inventoryTxtFiles/" + inventory.getName() + ".txt");
+            writer.write(inventory.getName() + "\n");
+            writer.write(inventory.getDescription() + "\n");
+            for (Product product : inventory.getInventory()) {
+                writer.write(product.getName() + "," + product.getQuantity() + "," + product.getDescription() + "\n");
+            }
+            writer.close();
+            System.out.println("Inventory file updated successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while updating the inventory file.");
+            e.printStackTrace();
+        }
     }
 }
